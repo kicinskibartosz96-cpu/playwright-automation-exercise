28dec62fd2a65fdeb2dc6c39f1119ab744708bfe
kicinskibartosz96 <kicinskibartosz96@gmail.com>
2026-05-28 10:50:25 +0200

    fix: dodanie naglowka User-Agent do ominiecia blokady 403 w chmurze

--- tests-api/auth-api.spec.js
+++ tests-api/auth-api.spec.js
@@ -3,8 +3,12 @@ import { test, expect } from '@playwright/test';
 test.describe('Testy API - Autoryzacja (Logowanie)', () => {
 
     test('Logowanie z poprawnymi danymi', async ({ request }) => {
-        // Dodana końcówka: /api/verifyLogin
         const response = await request.post('https://automationexercise.com', {
+            
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Accept': '*/*'
+            },
             form: { email: 'kicinskibartosz96@gmail.com', password: 'Bartosz132!' }
         });
         expect(response.status()).toBe(200);
@@ -14,8 +18,12 @@ test.describe('Testy API - Autoryzacja (Logowanie)', () => {
     });
 
     test('Logowanie ze złym hasłem wyświetla błąd', async ({ request }) => {
-        // Dodana końcówka: /api/verifyLogin
         const response = await request.post('https://automationexercise.com', {
+            
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Accept': '*/*'
+            },
             form: { email: 'kicinskibartosz96@gmail.com', password: 'wrongpassword' }
         });
         expect(response.status()).toBe(200);
@@ -24,4 +32,5 @@ test.describe('Testy API - Autoryzacja (Logowanie)', () => {
         expect(responseBody.message).toBe('User not found!');
     });
 });
+
 
